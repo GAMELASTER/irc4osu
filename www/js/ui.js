0dedf33b38d471d154a1f6cf8ab2a81d70f433ee
GAMELASTER <gamelaster@outlook.com>
2016-07-19 23:04:12 +0200

    Adding night mode + chat text formatting

--- www/js/ui.js
+++ www/js/ui.js
@@ -4,12 +4,9 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-var startTime = Date.now();
-
-setInterval(function() {
-  let elapsedTime = new Date(Date.now() - startTime - 36e5); //36e5 because one hour...
-  let hours = pad(elapsedTime.getHours(), 2);
-  let minutes = pad(elapsedTime.getMinutes(),2);
-  let seconds = pad(elapsedTime.getSeconds(),2);
-  $("#online-time-text").text(`${hours}:${minutes}:${seconds}`);
-}, 1100);
+function setNigthMode(enable) {
+  if(enable)
+    $("body").addClass("night");
+  else
+    $("body").removeClass("night");
+}
