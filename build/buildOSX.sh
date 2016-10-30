123a7681f0a3945afc2f73f3860d696f2584329e
Marek Kraus <gamieedevelop@outlook.sk>
2016-10-30 20:00:04 +0100

    Adding OS X build scripts

---
+++ build/buildOSX.sh
@@ -0,0 +1,2 @@
+#electron-packager .. irc4osu --platform=darwin --arch=x64 --asar --icon=../www/images/logo.icns --overwrite --prune --ignore=build/
+electron-packager .. irc4osu --platform=darwin --arch=x64 --icon=../www/images/logo.icns --prune --overwrite --ignore=build/
