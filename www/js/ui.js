4b0ef317f3232ae30b2efede0f88eafd0b8e2cac
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-11-03 15:35:43 +0100

    Added functional join channel modal

--- www/js/ui.js
+++ www/js/ui.js
@@ -68,7 +68,7 @@ const client = {
         message: message
       });
     });
-    
+
     this.irc.addListener("channellist", channelList => this.onChannelList(channelList));
   },
 
@@ -410,6 +410,38 @@ $(document).on("keyup", "#text-input", e => {
   }
 });
 
-$(document).on("click", "#openChannelsDialog", () => {
+// Open join channel modal
+$(document).on("click", "#open-channel-dialog", () => {
 
+  // Reset values
+  $("#select-channel-modal .channel-row").remove();
+  $("#channels-filter").val("");
+  
+  // Open dialog
+  $("#select-channel-modal").fadeIn(150);
+
+  client.channels.forEach((channelInfo) => {
+
+    // Collect all channel names we have open
+    let tabsList = client.tabs.map(e => e.name);
+
+    // Skip any channels we already have opened
+    if (channelInfo.name in tabsList) return;
+
+    // Append the html
+    $("#channels-list").append(
+      `<div data-channel="${channelInfo.name}" class="channel-row join-channel">
+         <h1>${channelInfo.name}</h1>
+         <p>${channelInfo.topic}<span class="active-users">${channelInfo.users} users</span></p>
+       </div>`);
+  }, this);
+});
+
+$(document).on("click", ".join-channel", e => {
+  client.joinChannel($(e.target).parent(".join-channel").data("channel"));
+  $("#select-channel-modal").fadeOut(150);
+});
+
+$(document).on("click", "#exit-select-channel-modal", e => {
+  $("#select-channel-modal").fadeOut(150);
 });
