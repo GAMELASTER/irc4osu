04e2b4e8f315e8bbf4aa9c731f61564e15855f23
Cubas Wiemer, Uwe <Uwe.Cubas-Wiemer@tecalliance.net>
2016-12-07 10:42:01 +0100

    Added initial irc functionality

--- app/components/client/client.component.js
+++ app/components/client/client.component.js
@@ -1,17 +1,45 @@
 angular = window.angular;
 
+const irc = require('irc');
+
 angular
 .module('irc4osu')
 .component('client', {
   templateUrl: './components/client/client.template.html',
   controller: function clientController() {
+
+    // Holds all the open channels
     this.channels = [
       { name: '#english' },
-      { name: '#german' },
       { name: '#osu' }
     ];
 
-    this.addTab = function () {
+    // Holds the currently opened channel
+    this.activeChannel = "#osu";
+
+    // Holds the user's name
+    this.username = "";
+
+    // Holds the irc client
+    this.irc = null;
+
+    // Initializes the client
+    this.init = function (username, password) {
+      // Start the client
+      this.irc = new irc.Client("irc.ppy.sh", username, {
+        password: password,
+        autoConnect: false
+      });
+
+      this.irc.connect(0, () => this.onConnected());
+    }
+
+    this.onConnected = function () {
+      console.log("Connected!");
+    }
+
+    // Joins a channel
+    this.joinChannel = function () {
       this.channels.push({name: '#announce'});
     }
   }
