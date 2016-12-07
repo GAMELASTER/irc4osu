f807000c54d30f7bc09fe80aab53263815739f57
Uwe Wiemer <hallowatcher@gmail.com>
2016-12-07 22:44:08 +0100

    Changed modal layout

--- app/modals/login/login.controller.js
+++ app/modals/login/login.controller.js
@@ -1,9 +1,34 @@
-var app = angular.module('irc4osu');
+const app = angular.module('irc4osu');
+const ircModule = require('irc');
 
-app.controller('loginController', ['$scope', 'close', function($scope, close) {
+app.controller('loginController', function($scope, channels, close) {
+  
+  $scope.ircClient = null;
 
-  $scope.close = function (result) {
- 	  close(result);
+  $scope.login = function (username, password) {
+
+    // Start the client
+    $scope.ircClient = new ircModule.Client("irc.ppy.sh", username, {
+      password: password,
+      channels: channels,
+      autoConnect: false
+    });
+
+    $scope.ircClient.on('error', (error) => {
+      switch (error.command) {
+
+      // Wrong password
+      case "err_passwdmismatch":
+        console.error('Invalid credentials!');
+        break;
+      }
+    });
+
+    // Listeners
+    $scope.ircClient.connect(0, () => {
+      console.log("connected");
+      close($scope.ircClient)
+    });
   };
 
-}]);
+});
